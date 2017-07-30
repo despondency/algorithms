f20b7471159048ca4b7eaa97518b433da662de2b
Mario Georgiev <cryingmario@gmail.com>
2017-07-30 19:22:23 +0300

    trigget ci

--- src/test/java/MergeSortTest.java
+++ src/test/java/MergeSortTest.java
@@ -2,14 +2,11 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MergeSortTest {
-
-		@Test
-		public void testMergeSort(){
-			MergeSort mergeSort = new MergeSort(new int[]{63,2,1,423,54,62,1,87});
+	  @Test
+		public void testMergeSort() {
+			MergeSort mergeSort = new MergeSort(new int[]{63, 2, 1, 423, 54, 62, 1, 87});
 			int actual[] = mergeSort.getSortedArray();
-			int expected[] = new int[]{1,1,2,54,62,63,87,423};
+			int expected[] = new int[]{1, 1, 2, 54, 62, 63, 87, 423};
 			Assert.assertArrayEquals(expected, actual);
 		}
-
-
 }
