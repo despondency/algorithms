fb183166876393bb36a10d100a13b1a709cf6f50
Mario Georgiev <cryingmario@gmail.com>
2017-07-30 19:21:08 +0300

    add travis.ci

---
+++ src/main/java/MergeSort.java
@@ -0,0 +1,34 @@
+public class MergeSort {
+
+	private int A[];
+	private int T[];
+	public MergeSort(int[] A){
+		this.A = A;
+		this.T = new int[A.length];
+		sort (A, 0, A.length-1);
+	}
+
+	private void sort(int A[], int L, int H){
+		if (L >= H) return;
+		int M = L + (H - L)/2;
+		sort (A, 0, M);
+		sort(A,M+1,H);
+		merge(A, L,M,H);
+	}
+
+	private void merge (int A[], int L,int M, int H){
+			System.arraycopy(A, L, T, L,H-L + 1);
+			int i = L;
+			int j = M+1;
+			for (int k = L; k <= H; k++){
+				if(i > M) A[k] = T[j++];
+				else if(j > H) A[k] = T[i++];
+				else if(T[j] < T[i]) A[k] = T[j++];
+				else A[k] = T[i++];
+			}
+	}
+
+	public int[] getSortedArray(){
+		return A;
+	}
+}
